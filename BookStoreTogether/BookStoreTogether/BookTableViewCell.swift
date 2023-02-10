1024c40c1e4cc24935a0b049c2cba04134fd13e0
max poffenbarger <maxpoffenbarger@gmail.com>
2023-02-10 09:10:14 -0700

    set ups custom cell on storyboard

---
+++ BookStoreTogether/BookStoreTogether/BookTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BookTableViewCell.swift
+//  BookStoreTogether
+//
+//  Created by Maxwell Poffenbarger on 2/10/23.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
